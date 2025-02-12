9824baebfefcad86a0adf3e17bf4e958e9235132
maryam23tariq <maryam23tariq@gmail.com>
2025-02-12 22:31:18 +0500

    mym

---
+++ lab 3/lab3_tb2.sv
@@ -0,0 +1,36 @@
+module full_adder_tb();
+ logic a1;
+ logic b1;
+ logic c1;
+ logic sum1;
+ logic carry1;
+
+ full_adder UUT (
+ .a(a1),
+ .b(b1),
+ .c(c1),
+ .sum(sum1),
+ .carry(carry1)
+ );
+
+ initial
+ begin
+ a1 = 0; b1 = 0; c1 = 0;
+ #10;
+ a1 = 0; b1 = 0; c1 = 1;
+ #10;
+ a1 = 0; b1 = 1; c1 = 0;
+ #10;
+ a1 = 0; b1 = 1; c1 = 1;
+ #10;
+ a1 = 1; b1 = 0; c1 = 0;
+ #10;
+ a1 = 1; b1 = 0; c1 = 1;
+ #10;
+ a = 1; b1 = 1; c1 = 0;
+ #10;
+ a1 = 1; b1 = 1; c1 = 1;
+ #10;
+ $stop;
+ 
+ endmodule
